68869db97a7fe28d3cc176fc50e737e5f3d7e51d
yusuke <sato11.jp@gmail.com>
2021-11-16 10:17:09 +0900

    create list.php t_list.php

---
+++ list.php
@@ -0,0 +1,9 @@
+<?php
+
+$title = '月間一覧';
+$links = [
+    'トップ' => 'index.php',
+    'ログアウト' => 'logout.php',
+];
+$content = 't_list.php';
+include 'views/layout.php';
---
+++ views/t_list.php
@@ -0,0 +1,54 @@
+<div class="select-term">
+    <form action="">
+        <p>期間選択</p>
+        <span></span>
+    </form>
+</div>
+<a href="register.php">新規登録</a>
+<table>
+    <thead>
+        <tr>
+            <th>日付</th>
+            <th>売上</th>
+            <th>売上累計</th>
+            <th>仕入れ</th>
+            <th>仕入れ累計</th>
+            <th>累計原価率</th>
+            <th>人件費</th>
+            <th>累計人件費</th>
+            <th>累計人件費率</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th>1</th>
+            <th>120,000</th>
+            <th>120,000</th>
+            <th>120,000</th>
+            <th>12,000</th>
+            <th>10%</th>
+            <th>15,000</th>
+            <th>15,000</th>
+            <th>20%</th>
+            <th>
+                <a href="update.php">修正</a>
+                <a href="delete.php">削除</a>
+            </th>
+        </tr>
+        <tr>
+            <th>2</th>
+            <th>120,000</th>
+            <th>120,000</th>
+            <th>120,000</th>
+            <th>12,000</th>
+            <th>10%</th>
+            <th>15,000</th>
+            <th>15,000</th>
+            <th>20%</th>
+            <th>
+                <a href="update.php">修正</a>
+                <a href="delete.php">削除</a>
+            </th>
+        </tr>
+    </tbody>
+</table>
